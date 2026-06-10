f5f3b6813f59915084754ee9ead748168c7299a3
Suriya Kumaran <suriyakumarananbazhagan@gmail.com>
2026-06-10 19:32:36 +0530

    Add files via upload

---
+++ Android-app/src/main/java/com/example/home/ConfigManager.java
@@ -0,0 +1,11 @@
+package com.example.home;
+
+/**
+ * Configuration manager for MQTT credentials and settings
+ * TODO: Move credentials to BuildConfig or environment variables for production
+ */
+public class ConfigManager {
+    public static final String MQTT_BROKER = "ssl://4348e232bb5545858370951f4fe2d907.s1.eu.hivemq.cloud:8883";
+    public static final String MQTT_USER = "Suriya";
+    public static final String MQTT_PASS = "Suriya6anbu6@#";
+}
